44b1c166294e3329e2b60b51d43307f446f471a5
Haziq Musa <last.game01987@gmail.com>
2022-07-21 20:28:03 +0800

    committed

--- App.js
+++ App.js
@@ -5,37 +5,170 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  Alert,
 } from "react-native";
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import {
+  getUser,
+  updateBalance,
+  updatCafeBalance,
+  getCafeBalance,
+} from "./firebase";
 
 const Stack = createNativeStackNavigator();
+export const UserContext = createContext();
+
+const ContextProvider = ({ children }) => {
+  const [value, setValue] = useState(null);
+  return (
+    <UserContext.Provider value={{ value, setValue }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="dashboard"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="login" component={Login} />
-        <Stack.Screen name="dashboard" component={Dashboard} />
-        <Stack.Screen name="QRScan" component={QRScanner} />
-        <Stack.Screen name="pay" component={Pay} />
-      </Stack.Navigator>
+      <ContextProvider>
+        <Stack.Navigator
+          initialRouteName="login"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="login" component={Login} />
+          <Stack.Screen name="dashboard" component={Dashboard} />
+          <Stack.Screen name="QRScan" component={QRScanner} />
+          <Stack.Screen name="pay" component={Pay} />
+        </Stack.Navigator>
+      </ContextProvider>
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
 
-const Pay = ({ route }) => {
-  const [amount, setAmount] = useState(0);
-  const { qrData } = route.params;
+const Login = ({ navigation }) => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [cafeOwner, setCafeOwner] = useState(false);
+  const { value, setValue } = useContext(UserContext);
+
+  const handleUserPromise = props => {
+    getUser(props)
+      .then(res => {
+        if (res.exist) {
+          setValue(res);
+          navigation.navigate("dashboard");
+        }
+      })
+      .catch(() => alert("Invalid account"));
+  };
+
+  const handleLogin = () => {
+    if (cafeOwner) {
+      handleUserPromise({ cafeId: id });
+    } else {
+      handleUserPromise({ matricNo: id });
+    }
+  };
+
+  return (
+    <View style={styles.loginContainer}>
+      <View style={{ marginBottom: 16 }}>
+        {cafeOwner ? (
+          <>
+            <Text>Cafe Id</Text>
+            <TextInput style={styles.input} onChangeText={setId} />
+          </>
+        ) : (
+          <>
+            <Text>Matric No.</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={setId}
+              placeholder="012345"
+            />
+          </>
+        )}
+      </View>
+      <View style={{ marginBottom: 32 }}>
+        <Text>Password</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          onChangeText={setPassword}
+        />
+      </View>
+      <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+        <Text style={styles.loginTxt}>Login</Text>
+      </TouchableOpacity>
+      <Text
+        onPress={() => setCafeOwner(!cafeOwner)}
+        style={{
+          marginTop: 16,
+          color: "gray",
+          textAlign: "center",
+          fontWeight: "500",
+        }}
+      >
+        {cafeOwner ? "Are you a student?" : "Are you a cafe owner?"}
+      </Text>
+    </View>
+  );
+};
+
+const Pay = ({ navigation }) => {
+  // const [amount, setAmount] = useState(0);
+  // const { qrData } = route.params;
+  const { value, setValue } = useContext(UserContext);
+
+  const handleTransfer = amount => {
+    // get balance from data
+    // balance - amount
+    // update in firestore
+    let balance = value.data.balance - amount;
+    updateBalance({ uid: value.id, value: balance })
+      .then(() => {
+        Alert.alert(
+          "Transfer successfully✅",
+          "Your e-wallet point will be deducted",
+          [{ text: "OK", onPress: () => navigation.navigate("dashboard") }]
+        );
+      })
+      .then(() => {
+        setValue({
+          data: {
+            balance: balance,
+            matricNo: value.data.matricNo,
+            name: value.data.name,
+          },
+          exist: value.exist,
+          id: value.id,
+        });
+      })
+      .then(() => {
+        getCafeBalance({ uid: "oSeP0fjoaYjTLh7vgr7L" })
+          .then(res => {
+            return res.data().balance;
+          })
+          .then(balance => {
+            let totalBal = parseInt(balance);
+            totalBal += parseInt(amount);
+
+            updatCafeBalance({
+              uid: "oSeP0fjoaYjTLh7vgr7L",
+              value: totalBal,
+            }).catch(err => console.error(err));
+          });
+      })
+      .catch(err => console.err(err));
+  };
 
   return (
     <View style={[styles.container, { justifyContent: "center" }]}>
-      <View>
+      {/* <View>
         <Text style={{ fontWeight: "600", color: "gray" }}>Enter Amount</Text>
         <View
           style={{
@@ -61,8 +194,56 @@ const Pay = ({ route }) => {
             placeholder="0.00"
           />
         </View>
+        <TouchableOpacity
+          style={{
+            marginTop: 24,
+            paddingVertical: 10,
+            backgroundColor: "black",
+            borderRadius: rounded,
+          }}
+          onPress={handleTransfer}
+        >
+          <Text
+            style={{ textAlign: "center", color: "white", fontWeight: "500" }}
+          >
+            Transfer
+          </Text>
+        </TouchableOpacity>
+      </View> */}
+      <Text style={{ fontWeight: "600", color: "gray", textAlign: "center" }}>
+        Choose Amount
+      </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          marginTop: 16,
+        }}
+      >
+        <AmountButton amount={1} mr={9} func={() => handleTransfer(1)} />
+        <AmountButton amount={2} func={() => handleTransfer(2)} />
       </View>
     </View>
+  );
+};
+
+const AmountButton = ({ amount, mr, func }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        // backgroundColor: "black",
+        borderWidth: 1,
+        borderRadius: rounded,
+        marginRight: mr,
+      }}
+      onPress={func}
+    >
+      <Text style={{ textAlign: "center", fontWeight: "500" }}>
+        RM {amount}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
@@ -146,12 +327,40 @@ const Dashboard = ({ navigation }) => {
       amount: "- RM2.00",
     },
   ];
+  const cafeTransaction = [
+    {
+      id: 1,
+      name: "Ali bin Abu",
+      time: "8.00am",
+      date: "18 July 2022",
+      amount: "+ RM2.00",
+    },
+    {
+      id: 2,
+      name: "Ahmad bin Ali",
+      time: "12.00pm",
+      date: "18 July 2022",
+      amount: "+ RM2.00",
+    },
+    {
+      id: 3,
+      name: "Aiman",
+      time: "5.00pm",
+      date: "18 July 2022",
+      amount: "+ RM2.00",
+    },
+  ];
+  const { value } = useContext(UserContext);
 
   return (
     <View style={styles.container}>
       <View style={{ flex: 1 }}>
         <View
-          style={{ marginTop: 15, flexDirection: "row", alignItems: "center" }}
+          style={{
+            marginTop: 32,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
         >
           <View
             style={{
@@ -163,29 +372,37 @@ const Dashboard = ({ navigation }) => {
             }}
           ></View>
           <View>
-            <Text style={{ fontSize: 13 }}>Ahmad bin Ali</Text>
-            <Text style={{ fontSize: 12, color: "gray" }}>012345</Text>
+            <Text style={{ fontSize: 13 }}>{value.data.name}</Text>
+            <Text style={{ fontSize: 12, color: "gray" }}>
+              {value.data.matricNo || value.data.cafeId}
+            </Text>
           </View>
         </View>
+
         <View style={styles.couponCard}>
           <Text style={{ color: "white", fontSize: 18 }}>e-Wallet</Text>
           <View>
-            <Text style={styles.balanceTxt}>My Balance</Text>
-            <Text style={styles.amountTxt}>RM 150.00</Text>
+            <Text style={styles.balanceTxt}>
+              {value.data.cafeId ? "Total" : "My Balance"}
+            </Text>
+            <Text style={styles.amountTxt}>RM {value.data.balance}</Text>
           </View>
         </View>
-        <TouchableOpacity
-          style={styles.transferBtn}
-          onPress={() => navigation.navigate("QRScan")}
-        >
-          <Text style={{ textAlign: "center", fontWeight: "500" }}>
-            Transfer💰
-          </Text>
-        </TouchableOpacity>
+        {value.data.matricNo && (
+          <TouchableOpacity
+            style={styles.transferBtn}
+            onPress={() => navigation.navigate("QRScan")}
+          >
+            <Text style={{ textAlign: "center", fontWeight: "500" }}>
+              Transfer💰
+            </Text>
+          </TouchableOpacity>
+        )}
       </View>
       <View style={{ flex: 2 }}>
         <Text
           style={{
+            marginLeft: 4,
             marginTop: 32,
             marginBottom: 5,
             fontWeight: "500",
@@ -194,19 +411,30 @@ const Dashboard = ({ navigation }) => {
         >
           Recent transaction
         </Text>
-        {transactionData.map(data => {
-          return (
-            <Transaction
-              key={data.id}
-              cafe={data.cafe}
-              time={data.time}
-              date={data.date}
-              amount={data.amount}
-            />
-          );
-        })}
+        {value.data.matricNo
+          ? transactionData.map(data => {
+              return (
+                <Transaction
+                  key={data.id}
+                  cafe={data.cafe}
+                  time={data.time}
+                  date={data.date}
+                  amount={data.amount}
+                />
+              );
+            })
+          : cafeTransaction.map(data => {
+              return (
+                <Transaction
+                  key={data.id}
+                  cafe={data.name}
+                  time={data.time}
+                  date={data.date}
+                  amount={data.amount}
+                />
+              );
+            })}
       </View>
-      <StatusBar style="auto" />
     </View>
   );
 };
@@ -224,54 +452,6 @@ const Transaction = ({ cafe, time, date, amount }) => {
         </View>
       </View>
       <Text style={{ fontWeight: "800" }}>{amount}</Text>
-    </View>
-  );
-};
-
-const Login = ({ navigation }) => {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-  const [cafeOwner, setCafeOwner] = useState(false);
-
-  return (
-    <View style={styles.loginContainer}>
-      <View style={{ marginBottom: 16 }}>
-        {cafeOwner ? (
-          <>
-            <Text>Cafe Id</Text>
-            <TextInput style={styles.input} onChangeText={setId} />
-          </>
-        ) : (
-          <>
-            <Text>Matric No.</Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={setId}
-              placeholder="012345"
-            />
-          </>
-        )}
-      </View>
-      <View style={{ marginBottom: 32 }}>
-        <Text>Password</Text>
-        <TextInput
-          style={styles.input}
-          secureTextEntry={true}
-          onChangeText={setPassword}
-        />
-      </View>
-      <TouchableOpacity
-        style={styles.loginBtn}
-        onPress={() => navigation.navigate("dashboard")}
-      >
-        <Text style={styles.loginTxt}>Login</Text>
-      </TouchableOpacity>
-      <Text
-        onPress={() => setCafeOwner(!cafeOwner)}
-        style={{ marginTop: 16, color: "gray", textAlign: "center" }}
-      >
-        Are you a cafe owner?
-      </Text>
     </View>
   );
 };
