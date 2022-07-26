7dbe739c6d85854037106def7abce0beb68fc549
Haziq Musa <last.game01987@gmail.com>
2022-07-26 12:27:56 +0800

    Change http status code

--- server/queries/studentsQueries.js
+++ server/queries/studentsQueries.js
@@ -2,7 +2,7 @@ const pool = require("./query");
 
 const getStudents = (request, response) => {
   pool.query("SELECT * FROM students", (error, results) => {
-    if (error) return response.status(401);
+    if (error) return response.sendStatus(500);
     return response.status(200).json(results.rows);
   });
 };
@@ -14,7 +14,8 @@ const getStudentsById = (request, response) => {
     "SELECT * FROM students WHERE matric_no = $1",
     [id],
     (error, results) => {
-      if (error) return response.status(404);
+      if (error) return response.sendStatus(500);
+      if (results.rowCount === 0) return response.sendStatus(404);
       return response.status(200).json(results.rows);
     }
   );
@@ -27,7 +28,8 @@ const createStudent = (request, response) => {
     "INSERT INTO students (matric_no, ic_no, student_name) VALUES ($1, $2, $3) RETURNING *",
     [matric_no, ic_no, name],
     (error, results) => {
-      if (error) return response.status(400);
+      if (error)
+        return response.status(500).send({ message: "Student already exist" });
       return response
         .status(201)
         .send(`User added with ID: ${results.rows[0].matric_no}`);
@@ -43,7 +45,8 @@ const setWalletAmount = (request, response) => {
     "UPDATE students SET wallet_amount = $1 WHERE matric_no = $2",
     [amount, id],
     (error, results) => {
-      if (error) return response.status(404);
+      if (error) return response.sendStatus(500);
+      if (results.rowCount === 0) return response.sendStatus(404);
       return response.status(200).send("Amount successfully set");
     }
   );
@@ -57,7 +60,8 @@ const suspendStudents = (request, response) => {
     "UPDATE students SET active = $1 WHERE matric_no = $2",
     [active, id],
     (error, results) => {
-      if (error) return response.status(404);
+      if (error) return response.sendStatus(500);
+      if (results.rowCount === 0) return response.sendStatus(404);
       return response
         .status(200)
         .send(`Students suspended with matric no: ${id}`);
