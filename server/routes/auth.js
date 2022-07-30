287b58ef37049298b9890d23fbfe75d54842be5f
Haziq Musa <last.game01987@gmail.com>
2022-07-30 20:21:58 +0800

    Adjust token expires time

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -76,7 +76,7 @@ const loginAdmin = (request, response) => {
 };
 
 const generateAccessToken = user =>
-  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15min" });
 
 router.post("/students/login", loginStudents);
 
