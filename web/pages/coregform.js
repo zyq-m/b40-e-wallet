c30928052f580327f2896974e97007fa1b269f29
Aqief Syahmi <aqiefsyahmi@gmail.com>
2022-08-05 21:57:11 +0800

    initial commit

---
+++ web/pages/coregform.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const coregform = () => {
+  return (
+    <div className="login min-h-screen grid place-items-center grid-rows-[max-content_1fr] bg-[#FFF7D9]">
+      <div className="justify-self-start">
+      </div>
+      <div>
+        <div className="p-9 rounded-2xl shadow-md bg-white w-[35rem]">
+          <h1 className="mb-8 font-bold text-3xl text-center">Cafe Owner Form Registration</h1>
+          <form>
+            <div className="items-center">
+              <Input type="fullname" placeholder="Full Name" />
+              <Input type="username" placeholder="Username" />
+              <Input type="cafename" placeholder="Cafe Name" />
+              <Input type="password" placeholder="Password" />
+            </div>
+            <button
+              type="submit"
+              className="mt-6 py-2 w-full font-medium bg-[#FFD400] rounded-md"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Input = ({ type, placeholder }) => {
+  return (
+    <>
+      <input
+        className="border w-full px-2 py-2 border-gray-300 rounded-md"
+        type={type}
+        placeholder={placeholder}
+      />
+    </>
+  );
+};
+
+export default coregform;
---
+++ web/pages/studregform.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const studregform = () => {
+  return (
+    <div className="login min-h-screen grid place-items-center grid-rows-[max-content_1fr] bg-[#FFF7D9]">
+      <div className="justify-self-start">
+      </div>
+      <div>
+        <div className="p-9 rounded-2xl shadow-md bg-white w-[35rem]">
+          <h1 className="mb-8 font-bold text-3xl text-center">Student Form Registration</h1>
+          <form>
+            <div className="items-center">
+              <Input type="fullname" placeholder="Full Name" />
+              <Input type="matricnum" placeholder="Matric Number" />
+              <Input type="icnum" placeholder="IC Number" />
+            </div>
+            <button
+              type="submit"
+              className="mt-6 py-2 w-full font-medium bg-[#FFD400] rounded-md"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Input = ({ type, placeholder }) => {
+  return (
+    <>
+      <input
+        className="border w-full px-2 py-2 border-gray-300 rounded-md"
+        type={type}
+        placeholder={placeholder}
+      />
+    </>
+  );
+};
+
+export default studregform;
