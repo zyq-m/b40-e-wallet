cdfd992e354a5a3da29a89ba523e152088e64fa1
Haziq Musa <last.game01987@gmail.com>
2022-08-08 19:58:03 +0800

    ganti index dgn login

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,23 +1,57 @@
-import { useState } from "react";
+import React from "react";
+import images from "../assets/icons/index";
+import logo from "../assets/logo-unisza.png";
 
-export default function Home() {
-  const [active, setActive] = useState(false);
-
+const login = () => {
   return (
-    <div className="grid place-items-center min-h-screen bg-gray-50">
+    <div className="login min-h-screen grid place-items-center grid-rows-[max-content_1fr] bg-[#FFF7D9]">
+      <div className="justify-self-start">
+        <img className="w-[13rem]" src={logo.src} alt="" />
+      </div>
       <div>
-        <h1 className="text-3xl font-bold">
-          {active ? "AUGHHHHHHHH🤯🥴😩" : "SHESHHHHHHHHH🔥🔥"}
-        </h1>
-        <div className="w-fit mx-auto">
-          <button
-            className="mt-4 px-5 py-3 font-semibold rounded-xl bg-slate-500 text-white "
-            onClick={() => setActive(!active)}
-          >
-            Press for AUGHH
-          </button>
+        <div className="p-9 rounded-2xl shadow-md bg-white w-[35rem]">
+          <h1 className="mb-8 font-bold text-4xl text-center">Welcome Back</h1>
+          <form>
+            <div className="grid gap-4 grid-cols-[max-content_1fr] items-center">
+              <img
+                className="justify-self-center w-[2.5rem]"
+                src={images.emailIcon.src}
+                alt=""
+              />
+              <Input type="email" placeholder="Enter Your Email..." />
+              <img
+                className="justify-self-center w-[2rem]"
+                src={images.passwordIcon.src}
+                alt=""
+              />
+              <Input type="password" placeholder="Enter Your Password..." />
+            </div>
+            <button
+              type="submit"
+              className="mt-6 py-2 w-full font-medium bg-[#FFD400] rounded-md"
+            >
+              Sign In
+            </button>
+          </form>
         </div>
+        <p className="text-center mt-4">
+          Copyright @unisza2022 | Privacy Policy
+        </p>
       </div>
     </div>
   );
-}
+};
+
+const Input = ({ type, placeholder }) => {
+  return (
+    <>
+      <input
+        className="border w-full px-2 py-2 border-gray-300 rounded-md"
+        type={type}
+        placeholder={placeholder}
+      />
+    </>
+  );
+};
+
+export default login;
