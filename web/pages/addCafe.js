3dd05492327c736eb2335c2f694b744fedd292ba
Aqief Syahmi <aqiefsyahmi@gmail.com>
2022-08-13 08:34:57 +0800

    initial commit

--- web/pages/addCafe.js
+++ web/pages/addCafe.js
@@ -24,7 +24,7 @@ const coregform = () => {
           <button
             type="submit"
             className="mt-6 py-2 w-full font-medium bg-[#FFD400] rounded-md"
-            onClick={() => router.push("/dashboard")}
+            onClick={() => router.push("/dashboard",  alert("New Cafe Owner Data Saved"))}
           >
             Register
           </button>
